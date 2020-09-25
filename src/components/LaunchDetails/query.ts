ae63a6033445f786e0a1a2af89db8a6a39fee25a
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-09-26 04:43:55 +0500

    add component folder

---
+++ src/components/LaunchDetails/query.ts
@@ -0,0 +1,24 @@
+import gql from 'graphql-tag';
+
+export const QUERY_LAUNCH_DETAILS = gql`
+    query launchInfo($id: String) {
+        launch(id: $id) {
+            flight_number
+            mission_name
+            launch_year
+            launch_success
+            details
+            launch_site {
+                site_name
+            }
+            rocket {
+                rocket_name
+                rocket_type
+            }
+            links {
+                video_link
+                flickr_images
+      }
+    }
+  }
+`;
